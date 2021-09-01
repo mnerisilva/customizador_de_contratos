73380cff6f1da3fa87c0a551da860ef831b3b347
mnerisilva <marceloneri@gmail.com>
2021-09-01 16:18:10 -0300

    update

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
+const areaTexto = document.querySelector('.area-texto');
 const check = document.querySelector('.form-check input');
 const nome = document.querySelector('.form #name');
 const natural = document.querySelector('.form #natural');
 const profissao = document.querySelector('.form #prof');
 
+if(check.getAttribute('checked')){
+    habilitaCampos()
+    areaTexto.setAttribute('contentEditable', false)
+}
+
+
 check.addEventListener("click", function(){
     if(check.checked == true){
-        nome.setAttribute('disabled', true);
-        nome.style.opacity = .2;
-        natural.setAttribute('disabled', true);
-        natural.style.opacity = .2;
-        profissao.setAttribute('disabled', true);
-        profissao.style.opacity = .2;
+        habilitaCampos()
+        areaTexto.setAttribute('contentEditable', false)
         return
     } 
-    nome.removeAttribute('disabled');
-    nome.style.opacity = 1;
-    natural.removeAttribute('disabled');
-    natural.style.opacity = 1;
-    profissao.removeAttribute('disabled');
-    profissao.style.opacity = 1;
+    desabilitaCampos()    
+    areaTexto.setAttribute('contentEditable', true)
 })
+
+function habilitaCampos(){
+        nome.removeAttribute('disabled', true);
+        nome.style.opacity = 1;
+        natural.removeAttribute('disabled', true);
+        natural.style.opacity = 1;
+        profissao.removeAttribute('disabled', true);
+        profissao.style.opacity = 1;
+}
+
+function desabilitaCampos(){
+    nome.setAttribute('disabled', true);
+    nome.style.opacity = .2;
+    natural.setAttribute('disabled', true);
+    natural.style.opacity = .2;
+    profissao.setAttribute('disabled', true);
+    profissao.style.opacity = .2;  
+}
