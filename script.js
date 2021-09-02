73b5ba99b34cd97b6398cd17dada75844cc5cf94
mnerisilva <marceloneri@gmail.com>
2021-09-02 16:25:33 -0300

    update

--- script.js
+++ script.js
@@ -42,3 +42,23 @@ function desabilitaCampos(){
     cpf.setAttribute('disabled', true);
     cpf.style.opacity = .2;  
 }
+
+
+
+function exportHTML(){
+    let header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' "+
+        "xmlns:w='urn:schemas-microsoft-com:office:word' "+
+        "xmlns='http://www.w3.org/TR/REC-html40'>"+
+        "<head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
+   let footer = "</body></html>";
+   let html = document.getElementById("source-html").innerHTML;
+   let sourceHTML = header+html+footer;
+   
+   let source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(sourceHTML);
+   let fileDownload = document.createElement("a");
+   document.body.appendChild(fileDownload);
+   fileDownload.href = source;
+   fileDownload.download = 'document.doc';
+   fileDownload.click();
+   document.body.removeChild(fileDownload);
+}
